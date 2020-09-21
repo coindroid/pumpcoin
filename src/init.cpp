5c394bdac8f0dc9a382e2460253acb186a01cbb3
Siziy Ivan <cziy@ya.ru>
2020-09-21 21:41:16 +0300

    remove zerocoin from QT

--- src/init.cpp
+++ src/init.cpp
@@ -752,16 +752,6 @@ bool AppInit2()
         return false;
     }
 
-    // ********************************************************* Testing Zerocoin
-
-
-    if (GetBoolArg("-zerotest", false))
-    {
-        printf("\n=== ZeroCoin tests start ===\n");
-        Test_RunAllTests();
-        printf("=== ZeroCoin tests end ===\n\n");
-    }
-
     // ********************************************************* Step 8: load wallet
 
     uiInterface.InitMessage(_("Loading wallet..."));
