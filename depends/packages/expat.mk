92f75c7a0c4d8ab46c74ff369f80a84cfb5eac6f
Your Name <you@example.com>
2020-10-02 23:34:17 +0200

    all here

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,6 +1,7 @@
 package=expat
 $(package)_version=2.1.0
-$(package)_download_path=http://sourceforge.net/projects/expat/files/expat/$($(package)_version)
+# https://nchc.dl.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz
+$(package)_download_path=https://nchc.dl.sourceforge.net/project/expat/expat/2.1.0
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=823705472f816df21c8f6aa026dd162b280806838bb55b3432b0fb1fcca7eb86
 
